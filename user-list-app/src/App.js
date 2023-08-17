3bc1ac495e14b46448b574ea273cdabdc8952e3f
Svobodennn <melih.sarac@hotmail.com>
2023-08-17 22:21:03 +0300

    User List Projesi

---
+++ user-list-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import UserList from './components/UserList.';
+import axios from 'axios';
+
+function App() {
+  return (
+    <div className="App">
+      <div><UserList/></div>
+      <div>Sağ</div>
+    </div>
+  );
+}
+
+export default App;
