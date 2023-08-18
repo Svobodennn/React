777638ddcc2b99c0e69675da975e46bc03c2bed6
Svobodennn <melih.sarac@hotmail.com>
2023-08-18 14:04:23 +0300

    Router Çalışmaları

---
+++ routing/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import {
+  Route,
+  Routes,
+  BrowserRouter
+} from "react-router-dom";
+
+//pages
+import Home from './pages/Home';
+import Users from './pages/Users';
+import Contact from './pages/Contact';
+import Menu from './components/Menu';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Menu/>
+      <Routes>
+          <Route index element={<Home />} />
+          <Route path='users' element={<Users />} />
+          <Route path='contact' element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
