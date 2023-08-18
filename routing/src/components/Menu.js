b17901df8ecceb1a75001a54c7cd00314bd84b75
Svobodennn <melih.sarac@hotmail.com>
2023-08-18 14:22:09 +0300

    useNavigate Fonksiyonu

--- routing/src/components/Menu.js
+++ routing/src/components/Menu.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 function Menu() {
+  const navigate = useNavigate();
   return (
     <div>
       <ul className='menu'>
         <li><Link to="/">Anasayfa</Link></li>
         <li><Link to="users">Kullanıcılar</Link></li>
         <li><Link to="contact">İletişim</Link></li>
+        <li onClick={()=>navigate("contact")}>İletişim (Navigate)</li>
+        <li><a href='#/' onClick={()=>navigate(-1)}>Geri</a></li>
+        <li onClick={()=>navigate(-1)}>Geri</li> {/* Bir önceki sayfaya dönmeyi sağlar */}
       </ul>
     </div>
   )
