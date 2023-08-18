0b537e138a05a9ddaaadc2ee82920651c3e4299f
Svobodennn <melih.sarac@hotmail.com>
2023-08-18 19:08:57 +0300

    useFormik Hook Kullanımı

--- routing/src/pages/Dashboard/Contact.js
+++ routing/src/pages/Dashboard/Contact.js
@@ -1,46 +1,110 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, useFormik } from 'formik';
 
 function Contact() {
+
+  const {handleSubmit, handleChange} = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      message: ""
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    }
+
+  });
+
   return (
     <div>
       <h2>Contact</h2>
 
-      <Formik
+      {/* <Formik //useFormik kullandık
         initialValues={{
           firstName: '',
           lastName: '',
           email: '',
+          message: ""
         }}
         onSubmit={(values) => {
           console.log(values);
         }}
-      >
+      > */}
 
-        { //böyle de olur
+      {
+
+
+        <form onSubmit={handleSubmit}>
+          <div>
+
+            <label htmlFor="firstName">First Name</label>
+            <input id="firstName" name="firstName" placeholder="Jane" onChange={(e) => handleChange('firstName')} />
+          </div>
+
+          <div>
+            <label htmlFor="lastName">Last Name</label>
+            <input id="lastName" name="lastName" placeholder="Doe" onChange={(e) => handleChange('lastName')} />
+          </div>
+
+          <div>
+            <label htmlFor="email">Email</label>
+
+            <input
+              id="email"
+              name="email"
+              placeholder="jane@acme.com"
+              type="email"
+              onChange={(e) => handleChange('email')}
+            />
+          </div>
+
+          <div>
+            <textarea name="message" id="message" placeholder='Mesajınız...' onChange={handleChange("message")}></textarea>
+            <button type="submit">Submit</button>
+
+          </div>
+        </form>
+
+      }
+      {/* { //böyle de olur | useFormik kullandık
           ({ handleSubmit, handleChange }) => (
 
             <form onSubmit={handleSubmit}>
+              <div>
+
               <label htmlFor="firstName">First Name</label>
               <input id="firstName" name="firstName" placeholder="Jane" onChange={(e)=>handleChange('firstName')}/>
+              </div>
 
+              <div>
               <label htmlFor="lastName">Last Name</label>
               <input id="lastName" name="lastName" placeholder="Doe" onChange={(e)=>handleChange('lastName')} />
-
+              </div>
+              
+              <div>
               <label htmlFor="email">Email</label>
+
               <input
                 id="email"
                 name="email"
                 placeholder="jane@acme.com"
                 type="email"
                 onChange={(e)=>handleChange('email')}
-              />
+                />
+              </div>
+
+              <div>
+              <textarea name="message" id="message" placeholder='Mesajınız...' onChange={handleChange("message")}></textarea>
               <button type="submit">Submit</button>
+
+              </div>
             </form>
           )
-        }
-        {/* <Form>
+        } */}
+
+      {/* <Form>
         <label htmlFor="firstName">First Name</label>
         <Field id="firstName" name="firstName" placeholder="Jane" />
 
@@ -56,7 +120,7 @@ function Contact() {
         />
         <button type="submit">Submit</button>
       </Form> */}
-      </Formik>
+      {/* </Formik> */}
     </div>
   )
 }
