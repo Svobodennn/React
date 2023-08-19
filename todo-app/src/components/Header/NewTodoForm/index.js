00f44f084e087380a12e5bfe55754493b82e5bd9
Svobodennn <melih.sarac@hotmail.com>
2023-08-19 17:36:13 +0300

    Formik ve Yup yüklendi. Form Bileşenleri Hazırlandı

---
+++ todo-app/src/components/Header/NewTodoForm/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Formik, Field, Form } from "formik";
+import validationSchema from './validation'
+
+
+function NewTodoForm() {
+
+
+    return (
+        <Formik
+            initialValues={{
+                text: "",
+            }}
+            onSubmit={(values, bag) => {
+                console.log(values)
+
+                bag.resetForm()
+            }}
+            validationSchema={validationSchema}
+        >
+
+            <Form>
+                <Field
+                    className='new-todo'
+                    placeholder='What needs to be done?'
+                    autoFocus
+                    id="text"
+                    name="text"
+                />
+            </Form>
+        </Formik>
+    )
+}
+
+export default NewTodoForm
