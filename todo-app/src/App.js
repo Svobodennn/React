726a0485b7db952d1592b56487087cd63017ac8f
Svobodennn <melih.sarac@hotmail.com>
2023-08-19 17:59:18 +0300

    Todo Context Oluşturulması

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,20 +1,21 @@
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { TodoProvider } from "./components/contexts/TodoContext";
 
 
 
 
 function App() {
   return (
-    <>
+    <TodoProvider>
       <section className="todoapp">
         <Header />
         <Content />
       </section>
       <Footer />
 
-    </>
+    </TodoProvider>
   );
 }
 
